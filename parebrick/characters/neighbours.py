462e4efa58298144f2874b43d839e1a7860323e2
Alexey Zabelkin <alexeyzabelkin@yandex.ru>
2023-05-04 17:38:16 +0300

    revert accidental neighbours.py change

--- parebrick/characters/neighbours.py
+++ parebrick/characters/neighbours.py
@@ -2,7 +2,6 @@ import os
 
 def write_trees_neightbours(blocks, ub_characters, neighbours, folder, show_branch_support, tree_holder, colors):
     for block, char in zip(blocks, ub_characters):
-        print('hey')
         labels = [f'{i}{"+" if i == len(colors) - 1 else ""} copies'
                   for i in range(max(char.values()) + 1)]
 
@@ -10,9 +9,7 @@ def write_trees_neightbours(blocks, ub_characters, neighbours, folder, show_bran
         tree_holder.count_innovations_fitch(char)
 
         ns_block = neighbours[block]
-
-        print(block, neighbours[block])
-        # if len(set([frozenset(ns) for ns in ns_block.values()])) == 1: continue
+        if len(set([frozenset(ns) for ns in ns_block.values()])) == 1: continue
 
         tree_holder.draw(file, legend_labels=labels, show_branch_support=show_branch_support, colors=colors,
                          neighbours=ns_block, neighbours_block=block, mode='r')
